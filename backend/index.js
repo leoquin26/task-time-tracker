c75745c9c10c2c1571e99c192c99364b7ea867a0
Leonardo Quintero <32395696+leoquin26@users.noreply.github.com>
2025-04-04 16:52:01 -0500

    fix

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Importa el módulo cors
 
 // Cargar variables de entorno
 dotenv.config();
 
 const app = express();
+
+// Configurar CORS para permitir solicitudes desde el frontend
+app.use(cors({
+  origin: 'https://task-time-tracker-g1kn.vercel.app', // Permite solo este origen
+  credentials: true // Si necesitas enviar cookies o credenciales
+}));
 
 // Middleware para parsear JSON
 app.use(bodyParser.json());
